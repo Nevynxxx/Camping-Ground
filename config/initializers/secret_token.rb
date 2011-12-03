87bc0bdf9c4a725ff9b05d743fdaaccbee1d6d8f
Anthony Metcalf <anthony@anferny.me.uk>
2011-12-03 10:16:31 +0000

    Added in Rails
    Keeping my README.md and removing the default readme. Used -T
    So that we don't get the normal test stuff as we will be using rspec.
    
    Fixes #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampingGround::Application.config.secret_token = 'e474a01a921e2b1d0a17abfdc8aa47f3657f1d5a2c04c5f132fe373890539ab313b3f0303a0b7892ab24a1b90544a0f737025fe63cf38d9f7443d3f3102dcb8f'
