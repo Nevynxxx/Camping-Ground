dad84c1295581b0f9bd7c9124705e7457eb393c7
Anthony Metcalf <nevyn@anferny.me.uk>
2011-12-05 10:47:36 +0000

    Commented Scenarios

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,8 +1,13 @@
+###
+# Scenario class, to hold the details of the individual
+# scenarios that we scrape from Paizo.com
+###
 class Scenario < ActiveRecord::Base
   belongs_to :author
 
   validates_presence_of :name, :number, :author_id, :season
 
+  # Returns the niely formated module number, including season
   def no
     "#%d-%02d" % [self.season, self.number]
   end
