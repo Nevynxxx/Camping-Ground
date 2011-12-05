81844090dacb3eba54f683b00cdf3a09493609e7
Anthony Metcalf <nevyn@anferny.me.uk>
2011-12-05 10:14:15 +0000

    Added comments to Author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,12 +1,19 @@
+###
+#The Author class. An Author is a writer of a scenario, and has the potential
+#to have many scenarios
+###
+
 class Author < ActiveRecord::Base
   validates_presence_of :firstname, :lastname
 
   #attr_accessor :firstname, :lastname
 
+  # As we don't store the full name, but use it, we search on first and last name
   def self.find_by_name(string)
     a = Author.find :first, :conditions => ["firstname = ? AND lastname = ?", string.split.first, string.split.last]
   end
 
+  # Returns a sensible value for to_s
   def to_s
     firstname + " " + lastname
   end
